cab368abdbf712c2fd225bbc6fee6764feb13e68
jiaawe <ch3njiawei@gmail.com>
2024-10-04 14:48:39 +0800

    download files + statistics notebook

---
+++ download_dataset.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Download the KuaiRec dataset
+wget https://nas.chongminggao.top:4430/datasets/KuaiRec.zip --no-check-certificate
+
+# Unzip the downloaded file
+unzip KuaiRec.zip
+
+# Clean up by removing the zip file
+rm KuaiRec.zip
+
+echo "Download and extraction complete in the data/ directory!"
